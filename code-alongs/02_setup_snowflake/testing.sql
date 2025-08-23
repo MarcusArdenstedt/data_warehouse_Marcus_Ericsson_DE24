2301f474fa8fbe560493114b15cf066a825a65b4
Marcus <marcus.ericsson_92@icloud.com>
2025-08-23 10:03:17 +0200

    First assignment, get a view over data

---
+++ code-alongs/02_setup_snowflake/testing.sql
@@ -0,0 +1,9 @@
+
+SHOW DATABASES;
+
+USE SCHEMA snowflake_sample_data.tpch_sf1;
+
+SHOW TABLES;
+
+
+SELECT count(*) AS "Amount_Brand#34" FROM PART WHERE P_BRAND = 'Brand#34';
---
+++ exercises/exercise_0_Introduction/assignment_0.sql
@@ -0,0 +1,10 @@
+
+-- a) Use this database and find out the underlying schemas, tables and views to get an overview of its logical structure
+
+SHOW DATABASES;
+
+USE GOOGLE_KEYWORDS_SEARCH_DATASET__DISCOVER_ALL_SEARCHES_ON_GOOGLE;
+
+ShOW SCHEMAS;
+
+SHOW VIEWS;
