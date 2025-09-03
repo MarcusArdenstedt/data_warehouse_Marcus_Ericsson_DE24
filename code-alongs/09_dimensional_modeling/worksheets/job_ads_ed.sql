509ec0e0303ea6fd17a0a59a44f1f4cfad5f4884
Marcus <marcus.ericsson_92@icloud.com>
2025-09-03 21:57:59 +0200

    code alongs 09 dimensional modeling

---
+++ code-alongs/09_dimensional_modeling/worksheets/job_ads_ed.sql
@@ -0,0 +1,46 @@
+
+SHOW ROLES;
+
+USE ROLE JOB_ADS_DLT_ROLE_3;
+
+USE SCHEMA job_ads_3.staging;
+
+
+SHOW TABLES;
+
+DESC TABLE data_field_job_ads;
+
+
+USE WAREHOUSE dev_wh;
+SELECT * FROM data_field_job_ads;
+
+SELECT 
+    relevance, 
+    workplace_address__street_address, 
+    workplace_address__postcode, 
+    publication_date,
+    webpage_url,
+    source_type
+FROM 
+    data_field_job_ads 
+LIMIT 5;
+
+SELECT 
+    scope_of_work__min,
+    scope_of_work__max,
+    employer__name,
+    salary_type__label,
+    salary_description,
+    duration__label,
+    logo_url,
+    employer__workplace,
+    employer__name,
+    employer__url
+FROM 
+    data_field_job_ads
+LIMIt 5;
+
+SELECT 
+    DISTINCT salary_description AS different_salary_descriptions
+FROM 
+    data_field_job_ads;
