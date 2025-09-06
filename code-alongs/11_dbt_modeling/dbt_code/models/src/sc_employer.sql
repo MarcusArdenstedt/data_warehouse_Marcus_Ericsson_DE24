492c5e1950aa178e5824621a879d1cd635aabca9
Marcus <marcus.ericsson_92@icloud.com>
2025-09-06 18:28:22 +0200

    first step with dbt, sources folder and two script in dim folder and removed dbt setups in live 08 setup dbt

---
+++ code-alongs/11_dbt_modeling/dbt_code/models/src/sc_employer.sql
@@ -0,0 +1,9 @@
+
+WITH stg_job_ads AS (SELECT * FROM {{ source('job_ads_3', 'stg_data_ads') }})
+
+SELECT 
+    id,
+    employer__name AS employer_name,
+    employer__workplace AS employer_workplace,
+    workplace_address__city AS workplace_city,
+FROM stg_job_ads
---
+++ code-alongs/11_dbt_modeling/dbt_code/models/src/src_job_ads.sql
@@ -0,0 +1,13 @@
+
+WITH stg_job_ads AS (SELECT * FROM {{ source('job_ads_3', 'stg_data_ads') }})
+
+SELECT 
+    id,
+    headline,
+    number_of_vacancies AS vacancies,
+    relevance,
+    application_deadline
+FROM 
+    stg_job_ads 
+ORDER BY 
+    application_deadline ASC;
